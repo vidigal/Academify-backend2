d5a1bf664ea52ad4c4bdc98cd50446df4d43388e
victorvidigal <victorvidigal@gmail.com>
2023-05-31 21:02:03 -0300

    authentication basic in memory

--- src/main/java/com/academify/AcademifyBackend2Application.java
+++ src/main/java/com/academify/AcademifyBackend2Application.java
@@ -2,12 +2,14 @@ package com.academify;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class AcademifyBackend2Application {
 
     public static void main(String[] args) {
         SpringApplication.run(AcademifyBackend2Application.class, args);
+        System.out.println(new BCryptPasswordEncoder().encode("123456"));
     }
 
 }
---
+++ src/main/java/com/academify/security/WebSecurityConfig.java
@@ -0,0 +1,41 @@
+package com.academify.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .httpBasic()
+                .and()
+                .authorizeHttpRequests()
+                .anyRequest().authenticated();
+
+        return httpSecurity.build();
+    }
+
+    @Bean
+    public AuthenticationManager authManager(HttpSecurity http) throws Exception {
+        AuthenticationManagerBuilder authenticationManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
+        authenticationManagerBuilder.inMemoryAuthentication().withUser("victor").password("$2a$10$BckPUI4.J3GDuEKNfRIT7OBHOrSBZVndXLI66A2S5R7aGYDHfT0mG").roles("ADMIN");
+        return authenticationManagerBuilder.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
